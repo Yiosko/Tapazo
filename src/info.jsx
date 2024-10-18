39ba96f395860e6015ba817ac346e94ceba3195a
Yiosko <diegoalejandrogutierrez77@gmail.com>
2024-10-17 22:48:36 -0500

    Add files via upload

---
+++ src/info.jsx
@@ -0,0 +1,27 @@
+function InfoPanel () {
+    return (
+        <>
+        <div className="info-panel-box">
+            <div className="panel-box">
+                <h2>Centro de información</h2>
+
+                <p>El tapazo consiste en un sorteo que se realiza en una reunion
+                  se toma la posicion de la tapa y se juega en una ruleta el ganador <br /><br />
+
+                  Esto se hace con el fin de recaudar dinero para fines de situacion de vulnerabilidad             
+                 </p> <br /><br /><br />
+
+
+                <h3>Precio de la tapa: 15 mil</h3>
+                <h3>Premio: 200 mil</h3>
+
+
+                
+                
+             </div>
+        </div>
+        </>
+    );
+}
+
+export default InfoPanel
