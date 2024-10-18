3afa29a00f563dcc9140e2a81b82cbc837cff7fa
Yiosko <diegoalejandrogutierrez77@gmail.com>
2024-10-17 22:46:31 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import InfoPanel from './info.jsx'
 
 function NumberTicket({value}) {
   const urlUno =`https://api.whatsapp.com/send?phone=+573212187011&text=Hola%2C+estoy+interesad%40+en+el+%2ATAPAZO%2A%F0%9F%98%8A%0A%0A_Quisiera+comprar+la+tapa+%2A%23${value+1}%2A_++%0A%0A%28INGRESE+SU+NOMBRE+%F0%9F%A4%93%2C+COMPROBANTE+DE+PAGO+A+NEQUI+%F0%9F%93%B2+%23+%2A3212187011%2A%60%60%60%28A+NOMBRE+DE+KAROLL%29%60%60%60%29.%0A%0A%0A%2AUna+vez+se+vea+reflejado+el+pago%2C+en+breve+se+les+asignaran+el+puesto.%2A%F0%9F%98%80%0A%0Ales+deseo+%2ASUERTE%2A+y+que+empiece+el+%2AJUEGO%2A+%F0%9F%91%BB`;
 
-  const urlDos =`https://wa.me/573212187011?text=Hola, estoy interesado en el *TAPAZO*😊%0a
-  me gustaria comprar la **TAPA#${value+2}**%0a%0a
+  const urlDos =`https://api.whatsapp.com/send?phone=+573212187011&text=Hola%2C+estoy+interesad%40+en+el+%2ATAPAZO%2A%F0%9F%98%8A%0A%0A_Quisiera+comprar+la+tapa+%2A%23${value+2}%2A_++%0A%0A%28INGRESE+SU+NOMBRE+%F0%9F%A4%93%2C+COMPROBANTE+DE+PAGO+A+NEQUI+%F0%9F%93%B2+%23+%2A3212187011%2A%60%60%60%28A+NOMBRE+DE+KAROLL%29%60%60%60%29.%0A%0A%0A%2AUna+vez+se+vea+reflejado+el+pago%2C+en+breve+se+les+asignaran+el+puesto.%2A%F0%9F%98%80%0A%0Ales+deseo+%2ASUERTE%2A+y+que+empiece+el+%2AJUEGO%2A+%F0%9F%91%BB`;
 
-  (en breve se negociara el metodo de pago) y se tomara en cuenta
-  `;
-  const urlTres =`https://wa.me/573212187011?text=Hola, estoy interesado en el *TAPAZO*😊%0a
-  me gustaria comprar la **TAPA#${value+3}**%0a%0a
+  const urlTres =`https://api.whatsapp.com/send?phone=+573212187011&text=Hola%2C+estoy+interesad%40+en+el+%2ATAPAZO%2A%F0%9F%98%8A%0A%0A_Quisiera+comprar+la+tapa+%2A%23${value+3}%2A_++%0A%0A%28INGRESE+SU+NOMBRE+%F0%9F%A4%93%2C+COMPROBANTE+DE+PAGO+A+NEQUI+%F0%9F%93%B2+%23+%2A3212187011%2A%60%60%60%28A+NOMBRE+DE+KAROLL%29%60%60%60%29.%0A%0A%0A%2AUna+vez+se+vea+reflejado+el+pago%2C+en+breve+se+les+asignaran+el+puesto.%2A%F0%9F%98%80%0A%0Ales+deseo+%2ASUERTE%2A+y+que+empiece+el+%2AJUEGO%2A+%F0%9F%91%BB`;
 
-  (en breve se negociara el metodo de pago) y se tomara en cuenta
-  `;
-  const urlCuatro =`https://wa.me/573212187011?text=Hola, estoy interesado en el *TAPAZO*😊%0a
-  me gustaria comprar la **TAPA#${value+4}**%0a%0a
+  const urlCuatro =`https://api.whatsapp.com/send?phone=+573212187011&text=Hola%2C+estoy+interesad%40+en+el+%2ATAPAZO%2A%F0%9F%98%8A%0A%0A_Quisiera+comprar+la+tapa+%2A%23${value+4}%2A_++%0A%0A%28INGRESE+SU+NOMBRE+%F0%9F%A4%93%2C+COMPROBANTE+DE+PAGO+A+NEQUI+%F0%9F%93%B2+%23+%2A3212187011%2A%60%60%60%28A+NOMBRE+DE+KAROLL%29%60%60%60%29.%0A%0A%0A%2AUna+vez+se+vea+reflejado+el+pago%2C+en+breve+se+les+asignaran+el+puesto.%2A%F0%9F%98%80%0A%0Ales+deseo+%2ASUERTE%2A+y+que+empiece+el+%2AJUEGO%2A+%F0%9F%91%BB`;
 
-  (en breve se negociara el metodo de pago) y se tomara en cuenta
-  `;
-  const urlCinco =`https://wa.me/573212187011?text=Hola, estoy interesado en el *TAPAZO*😊%0a
-  me gustaria comprar la **TAPA#${value+5}**%0a%0a
+  const urlCinco =`https://api.whatsapp.com/send?phone=+573212187011&text=Hola%2C+estoy+interesad%40+en+el+%2ATAPAZO%2A%F0%9F%98%8A%0A%0A_Quisiera+comprar+la+tapa+%2A%23${value+5}%2A_++%0A%0A%28INGRESE+SU+NOMBRE+%F0%9F%A4%93%2C+COMPROBANTE+DE+PAGO+A+NEQUI+%F0%9F%93%B2+%23+%2A3212187011%2A%60%60%60%28A+NOMBRE+DE+KAROLL%29%60%60%60%29.%0A%0A%0A%2AUna+vez+se+vea+reflejado+el+pago%2C+en+breve+se+les+asignaran+el+puesto.%2A%F0%9F%98%80%0A%0Ales+deseo+%2ASUERTE%2A+y+que+empiece+el+%2AJUEGO%2A+%F0%9F%91%BB`;
 
-  (en breve se negociara el metodo de pago) y se tomara en cuenta
-  `;
-  const urlSeis =`https://wa.me/573212187011?text=Hola, estoy interesado en el *TAPAZO*😊%0a
-  me gustaria comprar la **TAPA#${value+6}**%0a%0a
-
-  (en breve se negociara el metodo de pago) y se tomara en cuenta
-
-
-  `;
+  const urlSeis =`https://api.whatsapp.com/send?phone=+573212187011&text=Hola%2C+estoy+interesad%40+en+el+%2ATAPAZO%2A%F0%9F%98%8A%0A%0A_Quisiera+comprar+la+tapa+%2A%23${value+6}%2A_++%0A%0A%28INGRESE+SU+NOMBRE+%F0%9F%A4%93%2C+COMPROBANTE+DE+PAGO+A+NEQUI+%F0%9F%93%B2+%23+%2A3212187011%2A%60%60%60%28A+NOMBRE+DE+KAROLL%29%60%60%60%29.%0A%0A%0A%2AUna+vez+se+vea+reflejado+el+pago%2C+en+breve+se+les+asignaran+el+puesto.%2A%F0%9F%98%80%0A%0Ales+deseo+%2ASUERTE%2A+y+que+empiece+el+%2AJUEGO%2A+%F0%9F%91%BB`;
   return (
     <div>
       <button className='buttonTicket'><a href={urlUno} target='_blank'>{value + 1}</a></button>
@@ -47,6 +30,16 @@ function App() {
 
   const [canasta, setCanasta] = useState(true)
 
+  const [infor, useInfor] = useState(false)
+
+  const infoClick = () => {
+    if (infor){
+      useInfor(false)
+    } else if(infor === false){
+      useInfor(true)
+    }
+  }
+
   const handleClick = () =>{
     if(canasta){
     setCanasta(false)
@@ -57,13 +50,26 @@ function App() {
   return (
     <>
           <h1>TAPAZO</h1>
-
-
+          
+          
           <button onClick={handleClick}>CAMBIAR CANASTA</button>
+          <button onClick={infoClick}>?</button>
+          
+          {
+
+            infor === true && <>
+              <InfoPanel/>
+              <button onClick={infoClick} className='exit-panel'>X</button>
+            </>
+          }
+  
+          
           {
             canasta === true ?
             <>
             <h2>Canasta Uno</h2>
+
+            
             <NumberTicket value={0} />
             <NumberTicket value={6} />
             <NumberTicket value={12} />
