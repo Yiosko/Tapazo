881b6c8eefdda35a545f0afecdc0d46893ff5172
Yiosko <diegoalejandrogutierrez77@gmail.com>
2024-10-18 15:50:17 -0500

    Add files via upload

--- src/info.jsx
+++ src/info.jsx
@@ -9,11 +9,12 @@ function InfoPanel () {
                   se toma la posicion de la tapa y se juega en una ruleta el ganador <br /><br />
 
                   Esto se hace con el fin de recaudar dinero para fines de situacion de vulnerabilidad             
-                 </p> <br /><br /><br />
+                 </p> <br /><br />
 
 
-                <h3>Precio de la tapa: 15 mil</h3>
-                <h3>Premio: 200 mil</h3>
+                <h3>Precio de la tapa: 20 mil</h3>
+                <h3>Premio: 200 mil a la tapa mayor de la primera canasta</h3>
+                <h3>Premio: 100 mil a la tapa mas baja de la segunda canasta</h3>
 
 
                 
